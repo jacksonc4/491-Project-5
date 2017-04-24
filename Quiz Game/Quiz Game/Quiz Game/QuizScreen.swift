d5d7cc63a7df030e33cce6a1381ae3c1e7b92de6
Curtis Jackson <jacksonc4@vcu.edu>
2017-04-24 11:36:23 -0400

    Updated UI
    Need to fix constraints for labels on game page

--- Quiz Game/Quiz Game/Quiz Game/QuizScreen.swift
+++ Quiz Game/Quiz Game/Quiz Game/QuizScreen.swift
@@ -18,12 +18,29 @@ struct player {
 
 class QuizScreen: UIViewController {
     
+    @IBOutlet weak var p1Image: UIImageView!
+    @IBOutlet weak var p2Image: UIImageView!
+    @IBOutlet weak var p3Image: UIImageView!
+    @IBOutlet weak var p4Image: UIImageView!
+       
+    @IBOutlet weak var p1Score: UILabel!
+    @IBOutlet weak var p2Score: UILabel!
+    @IBOutlet weak var p3Score: UILabel!
+    @IBOutlet weak var p4Score: UILabel!
+    
+    var playerIcons:[UIImageView]!
+    var scoreLabels:[UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        playerIcons = [p1Image, p2Image, p3Image, p4Image]
+        scoreLabels = [p1Score, p2Score, p3Score, p4Score]
+        createPlayerIcons()
         showPlayers()
     }
     
+    //Change to be of type player
     var playerArray: [String] = []
     
     func showPlayers() {
@@ -35,11 +52,16 @@ class QuizScreen: UIViewController {
     }
     
     func createPlayerIcons() {
-        let iconHeight = 95
-        let iconWidth = 80
         
-        for i in 0..<playerArray.count {
-            //Draw the player icons
+        for player in playerIcons {
+            player.image = #imageLiteral(resourceName: "placeholderPlayerIcon.png")
+            player.alpha = 0.75
+                
+        }
+        
+        for score in scoreLabels {
+            score.text = "0"
+            score.alpha = 0.65
             
         }
         
